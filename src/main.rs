9f43923162dde4618f842d3100f210a682e0139f
atomloong <35839307+atomloong@users.noreply.github.com>
2025-01-22 00:21:00 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use deno_core::{anyhow::Result, JsRuntime, RuntimeOptions};
 
+#[tokio::main]
 async fn main()->Result<()>{
     let mut runtime = JsRuntime::new(RuntimeOptions::default());
     let code = r#"
