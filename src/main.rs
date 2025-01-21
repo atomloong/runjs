f0d16c550d14db03c9ebdce6a3cadd51d38987a0
atomloong <35839307+atomloong@users.noreply.github.com>
2025-01-22 00:01:25 +0800

    add basic files

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use deno_core::{anyhow::Result, JsRuntime, RuntimeOptions};
+
+async fn main()->Result<()>{
+    let mut runtime = JsRuntime::new(RuntimeOptions::default());
+    let code = r#"
+        Deno.core.print("Hello World");
+    "#;
+    runtime.execute_script("<anon>",code)?;
+    Ok(())
+}
